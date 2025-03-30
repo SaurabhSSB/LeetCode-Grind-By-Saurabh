6a4f1ccdffe2617779eae2c390164703ce35b27c
Saurabh Singh Bhandari <saurabhsinghbhandarissb@gmail.com>
2025-03-30 22:40:54 +0530

    Time: 12 ms (60.3%), Space: 18.3 MB (45.94%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,35 +1,15 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        x: str = str(k)
-        f= 0
-        if len(num) >= len(x):
-            size= len(x)
-        else:
-            size= len(num)
-            li= list(map(int,list(str(x))))
-            x= "".join(map(str,num))
-            num=li
-
-        for i in range(size):
+        carry = k
+        i = len(num) - 1
         
-            if num[-(i+1)] + int(x[-(i+1)]) > 9:
-                
-                if i+2 <= len(num):
-                    j=i
-
-                    while j+2 <= len(num) and num[-(j+2)]+ 1 > 9:
-                        num[-(j+1) - 1]= (num[-(j+1)- 1] + 1) % 10
-                        j+=1
-                    
-                    if j+2 <= len(num):    
-                        num[-(j+1)- 1]+= 1
-
-                    else:
-                        num.insert(0,1)
-
-                else:
-                    num.insert(0,1)
-
-            num[-(i+1)]= (num[-(i+1)] + int(x[-(i+1)])) % 10 
-
-        return(num)
+        while i >= 0 or carry > 0:
+            if i >= 0:
+                carry += num[i]
+                num[i] = carry % 10
+            else:
+                num.insert(0, carry % 10)
+            carry //= 10
+            i -= 1
+        
+        return num
